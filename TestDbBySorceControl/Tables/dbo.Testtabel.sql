eaf1dc126b5171dcfe329d7ec3179a0cefd85ecc
SheshoXd <mr.hashish@hotmail.com>
2019-01-01 14:00:06 +0200

    Create dbo.Testtabel.sql

---
+++ TestDbBySorceControl/Tables/dbo.Testtabel.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Testtabel]
+(
+[id] [int] NULL,
+[name] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[password] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
