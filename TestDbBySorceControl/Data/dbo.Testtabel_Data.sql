2cf8fd77d8f7ab55944c4d4f62938975093b7cfd
SheshoXd <mr.hashish@hotmail.com>
2019-01-01 14:10:51 +0200

    net

---
+++ TestDbBySorceControl/Data/dbo.Testtabel_Data.sql
@@ -0,0 +1,2 @@
+INSERT INTO [dbo].[Testtabel] ([id], [name], [password]) VALUES (1, N'sdsdd', N'qdsad')
+INSERT INTO [dbo].[Testtabel] ([id], [name], [password]) VALUES (2, N'dfddsf', N'sdfsd')
--- TestDbBySorceControl/Tables/dbo.Testtabel.sql
+++ TestDbBySorceControl/Tables/dbo.Testtabel.sql
@@ -1,7 +1,9 @@
 CREATE TABLE [dbo].[Testtabel]
 (
-[id] [int] NULL,
+[id] [int] NOT NULL,
 [name] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [password] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Testtabel] ADD CONSTRAINT [PK_Testtabel] PRIMARY KEY CLUSTERED  ([id]) ON [PRIMARY]
 GO
